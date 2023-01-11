07fcdea6443472169737f828c6ea3c6cf4d11228
victorperezmoreno <victor.perezmor@gmail.com>
2023-01-11 14:49:52 -0500

    Added fields to Users Table, Photo table created

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,11 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Extensions;
 
 namespace API.Entities
 {
-    public class AppUser
+  public class AppUser
     {
     public AppUser(string userName, byte[] passwordHash, byte[] passwordSalt)
     {
@@ -18,5 +17,21 @@ namespace API.Entities
     public string UserName { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string KnownAs { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public string Gender { get; set; }
+    public string Introduction { get; set; }
+    public string LookingFor { get; set; }
+    public string Interests { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public List<Photo> Photos { get; set; } = new();
+
+    public int GetAge()
+    {
+      return DateOfBirth.CalculateAge();
+    }
     }
 }
---
+++ API/Extensions/DateTimeExtension.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtension
+    {
+        public static int CalculateAge(this DateTime dob)
+        {
+          var today = DateTime.UtcNow.Date;
+          var age = today.Year - dob.Year;
+          if (dob > today.AddYears(-age)) age--;
+          return age;
+        }
+    }
+}
